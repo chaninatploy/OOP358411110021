992f913363897770ebdf90dbab521755b45d9209
Chaninatploy <Chaninat.ploy1996@gmail.com>
2018-11-19 16:35:05 +0700

    Lab 1 commit

--- src/lab1/intro.java
+++ src/lab1/intro.java
@@ -1,4 +1,18 @@
 package lab1;
 
 public class intro {
-}
+    public static void main(String[] args) {
+        System.out.println("chaninat janudom");
+        System.out.println("chaninat.ploy1996@gmail.com");
+        System.out.println("MT");
+        System.out.println("RMUTSV");
+
+
+
+    }//main
+
+
+
+
+
+}//class
