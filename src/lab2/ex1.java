25651a15890492218930ef04d6d531805764757f
Chaninatploy <Chaninat.ploy1996@gmail.com>
2018-11-26 14:34:20 +0700

    Lab 2: Variables

---
+++ src/lab2/ex1.java
@@ -0,0 +1,40 @@
+package lab2;
+
+public class ex1 {
+    public static void main(String[] args) {
+        //integer
+        int a;
+        a=10;
+        System.out.println(a);
+        int b = 20;
+        System.out.println(b);
+        int c=a+b;
+        System.out.println("ผลรวมของ a และ b เท่ากับ "+c);
+
+        //real number
+        double p;
+        p=5.5;
+        System.out.println(p);
+        p=p*10;
+        System.out.println(p);
+
+        //character
+        char d = '#';
+        System.out.println(d);
+
+        //String
+        String str = "Chaninat Janudom";
+        System.out.println("My Name is "+str);
+        System.out.println(str.length());
+        System.out.println(str.toUpperCase());
+
+
+
+
+    }//main
+
+
+
+
+
+}//class
