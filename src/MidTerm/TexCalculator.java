a2f89592c7019c5a21321d210585893e61d69265
Chaninatploy <Chaninat.ploy1996@gmail.com>
2019-01-28 14:15:41 +0700

    Lab 7 : Object & Class

---
+++ src/MidTerm/TexCalculator.java
@@ -0,0 +1,9 @@
+package MidTerm;
+
+public class TexCalculator {
+    public static void main(String[] args) {
+
+
+
+    }//main
+}//class
---
+++ src/lab7/StudentApp.java
@@ -0,0 +1,24 @@
+package lab7;
+
+public class StudentApp {
+    public static void main(String[] args) {
+        //object creation
+        //การสร้าง object
+        Student std1 = new Student();
+        System.out.println(std1.getName());
+        std1.setName("Boy");
+        System.out.println(std1.getName());
+        System.out.println(std1.getGender());
+        std1.setGender('M');
+        System.out.println(std1.getGender());
+        std1.setId("358411110021");
+        System.out.println(std1.getId());
+        /////////////////////////
+        Student std2 = new Student("22222222222222","Girl",'F');
+
+        System.out.println(std1.getName());
+        System.out.println(std2.getName());
+
+
+    }//main
+}//class
