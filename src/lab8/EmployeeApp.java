dfd3cf04dc2f2882a4d78efca8aa1df0321b4ed9
Chaninatploy <Chaninat.ploy1996@gmail.com>
2019-02-11 14:10:32 +0700

    Lab 8 : OOP-Inherit

---
+++ src/lab8/EmployeeApp.java
@@ -0,0 +1,16 @@
+package lab8;
+
+public class EmployeeApp {
+    public static void main(String[] args) {
+        //create objects
+        Employee emp =new Employee("1809700274129","Chaninat Janudom","Programmer",25000.00);
+
+        System.out.println(emp.toString());
+    }
+
+
+
+
+
+
+}//class
