b55d806a3f791f411f0951a8d9b10434c8ca7822
Chaninatploy <Chaninat.ploy1996@gmail.com>
2019-02-18 14:53:43 +0700

    Lab 8 :interface

---
+++ src/CaseStudy/BankApp.java
@@ -0,0 +1,18 @@
+package CaseStudy;
+
+public class BankApp {
+    public static void main(String[] args){
+        //object customer
+        Customer cus1 = new Customer();
+        cus1.setName("Chaninat Janudom");
+
+        //object bankAccount
+        bankAccount acc=new bankAccount("11-110-110-110",500.00,cus1);
+        System.out.println(acc.getInfo());
+        acc.doposit(2000.00);
+        System.out.println(acc.getInfo());
+
+        acc.withdraw(1000.00);
+        System.out.println(acc.getInfo());
+    }
+}//class
---
+++ src/CaseStudy/Customer.java
@@ -0,0 +1,14 @@
+package CaseStudy;
+
+public class Customer {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+}//class
