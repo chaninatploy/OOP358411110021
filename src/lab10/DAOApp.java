94ecc2a68cf26574eb28f3688fbfd8e19b92618f
Chaninatploy <Chaninat.ploy1996@gmail.com>
2019-03-04 16:09:11 +0700

    Lab 10 :DAO

--- src/lab10/DAOApp.java
+++ src/lab10/DAOApp.java
@@ -5,13 +5,37 @@ import java.util.List;
 public class DAOApp {
     public static void main(String[] args) {
         empDAOImpl dao = empDAOImpl.getInstant();
+        showAllData(dao);
 
+
+
+        //App_EMP
+        Employee newEmp = new Employee(102, "Panatda Sriphaoen", "System Analysis",30000);
+//        dao.addEmp(newEmp);
+//        showAllData(dao);
+
+        //FIND_BY_ID
+        Employee e = dao.getEmpById(100);
+        System.out.println(e.toString());
+
+        //UPDATE_EMP
+        e.setSalary(30000);
+        dao.updateEmp(e);
+        showAllData(dao);
+
+        //DELETE_EMP
+        dao.deleteEmp(100);
+        showAllData(dao);
+
+
+    }//main
+
+    private static void showAllData(empDAOImpl dao) {
         List<Employee> allEmp = dao.getAllEmp();
         System.out.println("-----Data in Database-----");
         for (Employee emp : allEmp) {
             System.out.println(emp.toString());
         }
         System.out.println("--------------------------");
-
-    }//main
+    }
 }//class
--- src/lab10/empDAOImpl.java
+++ src/lab10/empDAOImpl.java
@@ -12,6 +12,10 @@ public class empDAOImpl implements empDAO {
 
     //Constants Operation
     private static final String GET_ALL_EMP = "select * from employee";
+    private static final String ADD_EMP = "insert into employee (id, name, position, salary) values (?,?,?,?)";
+    private static final String FIND_BY_ID = "select * from employee where id = ?";
+    private static final String UPDATE_EMP = "update employee set name=? ,position=?,salary=? " + "Where id=?";
+    private static final String DELETE_EMP = "delete from employee where id=?";
 
     private static empDAOImpl instant = new empDAOImpl();
 
@@ -58,21 +62,91 @@ public class empDAOImpl implements empDAO {
 
     @Override
     public void addEmp(Employee emp) {
+        try {
+            conn = DriverManager.getConnection(url);
+            PreparedStatement ps = conn.prepareStatement(ADD_EMP);
+            ps.setInt(1,emp.getId());
+            ps.setString(2,emp.getName());
+            ps.setString(3,emp.getPosition());
+            ps.setDouble(4,emp.getSalary());
+            ps.execute();
+            System.out.println("Employee with name "+emp.getName()+"was add to database already. ");
+            ps.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public Employee getEmpById(int id) {
-        return null;
+        Employee emp = null;
+        try {
+            conn = DriverManager.getConnection(url);
+            PreparedStatement ps = conn.prepareStatement(FIND_BY_ID);
+            //set
+            ps.setInt(1,id);
+            ResultSet rs = ps.executeQuery();
+           if (rs.next()){
+                int idd = rs.getInt(1);
+                String name = rs.getString(2);
+                String position = rs.getString(3);
+                double salary = rs.getDouble(4);
+               emp = new Employee(idd, name, position, salary);
+            }else {System.out.println("Not found employee with id="+id);}
+            rs.close();
+           ps.close();
+           conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return emp;
     }
 
     @Override
     public void updateEmp(Employee emp) {
+        try {
+            conn = DriverManager.getConnection(url);
+            PreparedStatement ps = conn.prepareStatement(UPDATE_EMP);
+            ps.setString(1, emp.getName());
+            ps.setString(2, emp.getPosition());
+            ps.setDouble(3, emp.getSalary());
+            ps.setInt(4, emp.getId());
+
+            int result = ps.executeUpdate();
+            if (result !=0) {
+                System.out.println("Employee with id=" + emp.getId() + "was updated.");
+            }else {
+                System.out.println(""+"Could not update employee data with id="+emp.getId());
+            }
+            ps.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void deleteEmp(int id) {
+        try {
+            conn = DriverManager.getConnection(url);
+            PreparedStatement ps = conn.prepareStatement(DELETE_EMP);
+            ps.setInt(1,id);
+
+            int result = ps.executeUpdate();
+            if (result !=0) {
+                System.out.println("" + "Employee with id=" + id + "was deleted.");
+            }else {
+                System.out.println(""+"Could not deleted employee with id="+id);
+            }
+            ps.close();
+            conn.close();
+
+            } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
